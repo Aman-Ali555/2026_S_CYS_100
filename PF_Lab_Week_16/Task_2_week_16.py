a7fc2142a5c66a4ebc98bb8a0e6a7367f837abe5
Hafiz_Aman_Ali <amanhz0501@gmail.com>
2026-06-17 21:13:07 +0500

    Add files via upload

---
+++ PF_Lab_Week_16/Task_2_week_16.py
@@ -0,0 +1,5 @@
+def sq(n):
+    return n*n
+print(sq(5))
+a = lambda x:x*x
+print(a(5))
---
+++ PF_Lab_Week_16/Task_6_week_16.py
@@ -0,0 +1,6 @@
+L = [1,2,3,4]
+S = []
+for i in range(len(L)):
+     x = L[i]*L[i]
+     S.append(x)
+print(S)
